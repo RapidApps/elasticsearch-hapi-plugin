2b09d49840d2840cb4e00da591a28635d5220b39
Josiah <josiah@rapidapps.io>
2014-10-10 05:22:59 +0100

    Special handling for elastic search errors sent in the reply

--- plugin.js
+++ plugin.js
@@ -11,6 +11,8 @@ exports.register.attributes = {
 };
 
 function register(plugin, options, next) {
+  var Hapi = plugin.hapi;
+
   var config = {
     log: new Logger(plugin)
   };
@@ -20,6 +22,56 @@ function register(plugin, options, next) {
   }
 
   var es = new ElasticSearch.Client(config);
+
+  plugin.ext('onPostHandler', function (request, reply) {
+    var response = request.response;
+
+    if (response instanceof ElasticSearch.errors.ConnectionFault) {
+      return reply(Hapi.error.serverTimeout(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.NoConnections) {
+      return reply(Hapi.error.serverTimeout(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.RequestTimeout) {
+      return reply(Hapi.error.gatewayTimeout(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.ServiceUnavailable) {
+      return reply(Hapi.error.serverTimeout(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.InternalServerError) {
+      return reply(Hapi.error.badImplementation(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.PreconditionFailed) {
+      return reply(Hapi.error.preconditionFailed(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.Conflict) {
+      return reply(Hapi.error.conflict(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.Forbidden) {
+      return reply(Hapi.error.forbidden(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.NotFound) {
+      return reply(Hapi.error.notFound(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.BadRequest) {
+      return reply(Hapi.error.badRequest(response.message));
+    }
+
+    if (response instanceof ElasticSearch.errors.Generic) {
+      return reply(Hapi.error.badImplementation(response.message));
+    }
+
+    reply();
+  });
 
   plugin.expose('es', es);
   return next();
--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,32 +1,170 @@
+'use strict';
+
 var Lab = require('lab');
-var Hapi = require('hapi');
+var Server = require('hapi').Server;
+var Boom = require('hapi').error;
+var ElasticSearch = require('elasticsearch');
 var ElasticSearchPlugin = require('../plugin');
 
 var lab = exports.lab = Lab.script();
 
 lab.experiment('ElasticSearch Hapi Plugin', function () {
-  var server;
-  var pack;
-  var plugin;
+  lab.test('elastic search client on the `es` property', function (next) {
+    var server = new Server(0);
+    server.pack.register(ElasticSearchPlugin, function (err) {
+      if (err) return next(err);
 
-  lab.before(function (next) {
-    server = new Hapi.Server(0);
-    pack = server.pack;
-    next();
-  });
-
-  lab.test('is registered with a hapi server pack', function (next) {
-    pack.register({
-      plugin: ElasticSearchPlugin,
-      options: {
-        keepAlive: false
-      }
-    }, function (err) {
-      Lab.expect(err).to.not.exist;
-
-      plugin = pack.plugins['elasticsearch-hapi-plugin'];
+      var plugin = server.pack.plugins['elasticsearch-hapi-plugin'];
       Lab.expect(plugin).to.have.property('es');
       next();
+    });
+  });
+
+  lab.test('all plugin options passed to elastic search client', function (next) {
+    var server = new Server(0);
+    server.pack.register({
+      plugin: ElasticSearchPlugin,
+      options: {
+        apiVersion: "1.1"
+      }
+    }, function (err, pack) {
+      if (err) return next(err);
+
+      var es = server.pack.plugins['elasticsearch-hapi-plugin'].es;
+      Lab.expect(es.transport._config.apiVersion).to.equal('1.1');
+      next();
+    });
+  });
+
+  lab.experiment('elastic search HTTP errors', function (next) {
+    var server;
+    var pack;
+
+    lab.before(function (next) {
+      server = new Server(0, '127.0.0.1', {debug: false});
+
+      function ErrorRoute(path, Error) {
+        this.method = 'GET';
+        this.path = path;
+        this.handler = function (request, reply) {
+          reply(new Error());
+        };
+      }
+
+      server.route({
+        method: 'GET',
+        path: '/ok',
+        handler: function (request, reply) {
+          reply({success: true});
+        }
+      });
+
+      server.route(new ErrorRoute('/connection-fault', ElasticSearch.errors.ConnectionFault));
+      server.route(new ErrorRoute('/no-connections', ElasticSearch.errors.NoConnections));
+      server.route(new ErrorRoute('/request-timeout', ElasticSearch.errors.RequestTimeout));
+      server.route(new ErrorRoute('/service-unavailable', ElasticSearch.errors.ServiceUnavailable));
+      server.route(new ErrorRoute('/internal-server-error', ElasticSearch.errors.InternalServerError));
+      server.route(new ErrorRoute('/precondition-failed', ElasticSearch.errors.PreconditionFailed));
+      server.route(new ErrorRoute('/conflict', ElasticSearch.errors.Conflict));
+      server.route(new ErrorRoute('/forbidden', ElasticSearch.errors.Forbidden));
+      server.route(new ErrorRoute('/not-found', ElasticSearch.errors.NotFound));
+      server.route(new ErrorRoute('/bad-request', ElasticSearch.errors.BadRequest));
+      server.route(new ErrorRoute('/generic', ElasticSearch.errors.Generic));
+
+      pack = server.pack;
+      pack.register(ElasticSearchPlugin, function (err) {
+        if (err) return next(err);
+
+        server.start(next);
+      });
+    });
+
+    lab.after(function (next) {
+      server.stop(next);
+    });
+
+    lab.test('No error', function (next) {
+      server.inject('/ok', function (response) {
+        Lab.expect(response.statusCode).to.equal(200);
+        next();
+      });
+    });
+
+    lab.test('503 Connection fault', function (next) {
+      server.inject('/connection-fault', function (response) {
+        Lab.expect(response.statusCode).to.equal(503);
+        next();
+      });
+    });
+
+    lab.test('503 No Connections', function (next) {
+      server.inject('/no-connections', function (response) {
+        Lab.expect(response.statusCode).to.equal(503);
+        next();
+      });
+    });
+
+    lab.test('504 Request Timeout', function (next) {
+      server.inject('/request-timeout', function (response) {
+        Lab.expect(response.statusCode).to.equal(504);
+        next();
+      });
+    });
+
+    lab.test('503 Service Unavailable', function (next) {
+      server.inject('/service-unavailable', function (response) {
+        Lab.expect(response.statusCode).to.equal(503);
+        next();
+      });
+    });
+
+    lab.test('500 Internal Server Error', function (next) {
+      server.inject('/internal-server-error', function (response) {
+        Lab.expect(response.statusCode).to.equal(500);
+        next();
+      });
+    });
+
+    lab.test('412 Precondition Failed', function (next) {
+      server.inject('/precondition-failed', function (response) {
+        Lab.expect(response.statusCode).to.equal(412);
+        next();
+      });
+    });
+
+    lab.test('409 Conflict', function (next) {
+      server.inject('/conflict', function (response) {
+        Lab.expect(response.statusCode).to.equal(409);
+        next();
+      });
+    });
+
+    lab.test('403 Forbidden', function (next) {
+      server.inject('/forbidden', function (response) {
+        Lab.expect(response.statusCode).to.equal(403);
+        next();
+      });
+    });
+
+    lab.test('404 Not Found', function (next) {
+      server.inject('/not-found', function (response) {
+        Lab.expect(response.statusCode).to.equal(404);
+        next();
+      });
+    });
+
+    lab.test('400 Bad Request', function (next) {
+      server.inject('/bad-request', function (response) {
+        Lab.expect(response.statusCode).to.equal(400);
+        next();
+      });
+    });
+
+    lab.test('500 Generic', function (next) {
+      server.inject('/generic', function (response) {
+        Lab.expect(response.statusCode).to.equal(500);
+        next();
+      });
     });
   });
 });
